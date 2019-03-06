b741ed45a16b07eca3ce495f1818b89b3ee40d57
Marcell Vazquez-Chanlatte <mvc@linux.com>
2019-03-05 23:10:40 -0800

    reworked gridworld to use 1hot encodings and shifts

--- aiger_gridworld/dynamics.py
+++ aiger_gridworld/dynamics.py
@@ -1,4 +1,5 @@
-import aigerbv
+from aigerbv import atom, ite, split_gate
+
 
 
 NORTH = 0
@@ -7,15 +8,24 @@ SOUTH = 2
 WEST = 3
 
 
-def _gridworld1d(n, state_name='x', action='a', start=0):
-    x = aigerbv.atom(n, state_name, signed=False)
-    a = aigerbv.atom(n, action)
+def chain(n, state_name='x', action='a', start=None, clip=True, can_stay=True):
+    if start is None:
+        start = 1 << (n // 2)
+    x = atom(n, state_name, signed=False)
+    a = atom(2, action, signed=False)
 
-    guard = (a != 1) | (x != 2**n - 1)
-    guard &= (a != -1) | (x != 0)
-    x += guard.repeat(n) & a
+    backward, forward = a[0], a[1]
+    x2 = ite(forward, x << 1, x >> 1)
 
-    circ = x.aigbv['o', {x.output: state_name}]
+    stay = atom(1, 1, signed=False)
+    if clip:
+        stay = (x2 == 0)
+    if can_stay:
+        stay |= ~(forward | backward)
+    if clip or can_stay:
+        x2 = ite(stay, x, x2)
+
+    circ = x2.aigbv['o', {x2.output: state_name}]
     return circ.feedback(
         inputs=[state_name],
         outputs=[state_name],
@@ -24,23 +34,6 @@ def _gridworld1d(n, state_name='x', action='a', start=0):
     )
 
 
-def gridworld(n, start=(0, 0)):
-    circ = _gridworld1d(n, 'x', 'ax', start[0]) \
-        | _gridworld1d(n, 'y', 'ay', start[1])
-
-    ax = aigerbv.lookup(mapping={0: 0, 1: 1, 2: 0, 3: -1},
-                        input='tmp1',
-                        output='ax',
-                        inlen=2,
-                        outlen=n,
-                        in_signed=False)
-
-    ay = aigerbv.lookup(mapping={0: 1, 1: 0, 2: -1, 3: 0},
-                        input='tmp2',
-                        output='ay',
-                        inlen=2,
-                        outlen=n,
-                        in_signed=False)
-
-    action = aigerbv.tee(2, {'a': ('tmp1', 'tmp2')}) >> (ax | ay)
-    return action >> circ
+def gridworld(n, start=(None, None)):
+    circ = chain(n, 'x', 'ax', start[0]) | chain(n, 'y', 'ay', start[1])
+    return split_gate('a', 2, 'ax', 2, 'ay') >> circ
